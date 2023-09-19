1ebefb4ddd992b0a23eb4f616c109f5cff8e180f
maxi032 <maxxyro@yahoo.com>
2023-09-19 14:35:48 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # laravel-duplicate-model
-WIP Package to duplicate a model and its relations
+WIP Package to duplicate a model and its relations.
---
+++ src/LaravelDuplicateModelServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Maxi032\LaravelAdminPackage;
+
+
+use Illuminate\Database\ConnectionInterface;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDuplicateModelServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+
+    }
+
+    public function boot(): void
+    {
+
+    }
+}
